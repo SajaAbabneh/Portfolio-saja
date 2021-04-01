293b0c20881d5659b2460cd546183cc697517eb3
SajaAbabneh <ababnehsaja@yahoo.com>
2021-04-01 15:36:49 -0700

    lab5

---
+++ server.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const express = require('express');
+
+const server = express();
+const PORT = process.env.PORT || 3030;
+
+server.use(express.static('./public'));
+
+server.get('/',(req,res)=>{
+    res.send('home route')
+})
+
+server.get('/test',(request,response)=>{
+    response.send('You server is alive')
+})
+
+
+server.listen (PORT,()=>{
+    console.log(`Listening on PORT ${PORT}`);
+})
